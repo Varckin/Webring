0694fc824501d753f4a058c1b9624d6a50c89912
Markus Varckin XYZ <varckin@protonmail.com>
2025-02-17 14:56:31 +0300

    delete last_update from serializer api. Update home page html.

--- api/serializers.py
+++ api/serializers.py
@@ -4,12 +4,4 @@ from .models import WebringModel
 class WebringSerializer(serializers.ModelSerializer):
     class Meta:
         model = WebringModel
-        fields = ['name', 'status', 'last_updated', 'description', 'url']
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-
-        if self.context.get('request').method == 'GET':
-            representation.pop('last_updated', None)
-
-            return representation
+        fields = ['name', 'status', 'description', 'url']
